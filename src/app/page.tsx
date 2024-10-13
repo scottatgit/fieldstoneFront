a539746975f80d166c77f8d4ec58ed7bfa777b7f
Scott Everett <s.everett@fieldstone.pro>
2024-10-13 15:01:41 -0500

    removed welcome modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,14 @@
-"use client"
-import { Suspense, useEffect, useState } from "react";
+"use client";
+import { Suspense, useState } from "react";
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
-import WelcomeModal from "../components/WelcomeModal";
 import LoginModal from "../components/LoginModal";
 import RegisterModal from "../components/RegisterModal";
 import Acquisition from "./Acquisition"; // Updated to use Acquisition
 
 export default function Page() {
-  const [isWelcomeOpen, setIsWelcomeOpen] = useState(false);
   const [isLoginOpen, setLoginOpen] = useState(false);
   const [isRegisterOpen, setRegisterOpen] = useState(false);
-
-  useEffect(() => {
-    const welcomeDismissed = sessionStorage.getItem("welcomeDismissed");
-    if (!welcomeDismissed) {
-      setIsWelcomeOpen(true);
-    }
-  }, []);
-
-  const handleCloseWelcome = () => {
-    setIsWelcomeOpen(false);
-    sessionStorage.setItem("welcomeDismissed", "true");
-  };
 
   const handleLoginOpen = () => setLoginOpen(true);
   const handleRegisterOpen = () => setRegisterOpen(true);
@@ -33,14 +19,6 @@ export default function Page() {
         <Suspense fallback={<div>Loading...</div>}>
           <Acquisition /> {/* Use Acquisition component here */}
         </Suspense>
-
-        {/* Welcome Modal 
-        <WelcomeModal
-          isOpen={isWelcomeOpen}
-          onClose={handleCloseWelcome}
-          onLoginOpen={handleLoginOpen}
-          onRegisterOpen={handleRegisterOpen}
-        />*/}
 
         {/* Login and Register Modals */}
         <LoginModal isOpen={isLoginOpen} onClose={() => setLoginOpen(false)} />
